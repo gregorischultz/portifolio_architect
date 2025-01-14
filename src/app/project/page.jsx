2f6bf8e6044f117ea31732881784bb1496e9cc69
Grégori <gregorischultz07@gmail.com>
2025-01-14 19:33:03 +0100

    add pages

---
+++ src/app/project/page.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Projetos() {
+    return(
+        <>
+        <h1>Eu sou Projetos</h1>
+        </>
+    )
+}
---
+++ src/app/servicos/page.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Serviços() {
+    return(
+        <>
+        <h1>Eu sou Serviços</h1>
+        </>
+    )
+}
