e3fb7d294db7b9715255e97493cb7c2371ab3e16
Grégori <gregorischultz07@gmail.com>
2025-01-14 19:22:22 +0100

    navigation bar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./styles/page.module.css";
+import NavBar from "./componentes/HomePage/NavBar";
 
 export default function Home() {
   return (
     <>
-    
+    <NavBar/>
     </>
   );
 }
