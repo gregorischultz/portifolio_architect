26f962f50158bb114a9889fb14fef4cc0ab34d3f
Grégori <gregorischultz07@gmail.com>
2025-01-20 20:42:30 +0100

    services component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import styles from "./styles/page.module.css";
 import NavBar from "./componentes/NavBar";
 import Footer from "./componentes/Footer";
 import Hero from "./componentes/HomePage/Hero";
+import Services from "./componentes/HomePage/Services";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <NavBar />
       <Hero />
+      <Services />
       <Footer />
     </div>
 
