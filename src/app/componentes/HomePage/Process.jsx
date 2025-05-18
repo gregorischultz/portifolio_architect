c6c1815c2276b7f7047c40476a2f8bead11fd668
Grégori <gregorischultz07@gmail.com>
2025-05-18 16:39:06 +0200

    start of changes to the Process component

--- src/app/componentes/HomePage/Process.jsx
+++ src/app/componentes/HomePage/Process.jsx
@@ -1,55 +1,31 @@
 
-import styles from "@/app/styles/Process.module.css";
+import styles from '@/app/styles/Process.module.css';
 import ProcessCard from "./ProcessCard";
 
 
 export default function Process() {
-    const cards = [
-        {
-            numberImg: '/1..png',
-            title: 'Exploração e Briefing',
-            description:
-                'Partilhe connosco as suas ideias, objetivos e expectativas. A nossa equipa estará atenta a cada detalhe para compreender a sua visão.',
-        },
-        {
-            numberImg: '/2.png',
-            title: 'Concepção e Modelação 3D',
-            description:
-                'Com base no briefing, desenvolvemos modelos 3D detalhados e precisos, assegurando que todos os aspectos do projecto estarão fielmente representados.',
-        },
-        {
-            numberImg: '/3.png',
-            title: 'Renderização de Alta Qualidade',
-            description:
-                'Criamos imagens e vídeos realistas que dão vida ao projecto, capturando a essência e o impacto que deseja transmitir.',
-        },
-        {
-            numberImg: '/4.png',
-            title: 'Revisões e Ajustes Personalizados',
-            description:
-                'Valorizamos o seu feedback e ajustamos os detalhes para garantir que o resultado final corresponda exatamente ao que foi idealizado.',
-        },
-        {
-            numberImg: '/5.png',
-            title: 'Entrega Final e Suporte',
-            description:
-                'Apresentamos o produto final em formatos práticos e adequados à sua utilização, oferecendo suporte para aplicações futuras e novas ideias.',
-        },
-    ];
-
     return (
-        <section className={styles.process}>
+        <section className={styles.container}>
             <h2 className={styles.title}>Nosso Processo</h2>
-            <p className={styles.description}>Cada renderização é mais do que uma imagem, é uma experiência visual que comunica, convence, encanta e desperta a imaginação.</p>
-            <div className={styles.cards}>
-                {cards.map((card, index) => (
-                    <ProcessCard
-                        key={index}
-                        numberImg={card.numberImg}
-                        title={card.title}
-                        description={card.description}
-                    />
-                ))}
+            <p className={styles.description}>
+                Cada renderizaçao é mais do que uma imagem, é uma experiencia visual que comunica, convence, encana e despeta a <span className={styles.highlight}>imaginaçao</span>
+            </p>
+            <div className={styles.steps}>
+                <article className={`${styles.card} ${styles.card1}`}>
+                    <h3>1. Exploraçao e briefing</h3>
+                </article>
+                <article className={`${styles.card} ${styles.card2}`}>
+                    <h3>2. Concepção e Modelação 3D</h3>
+                </article>
+                <article className={`${styles.card} ${styles.card3}`}>
+                    <h3>3. Renderização de Alta Qualidade</h3>
+                </article>
+                <article className={`${styles.card} ${styles.card4}`}>
+                    <h3>4. Revisões e Ajustes Personalizado</h3>
+                </article>
+                <article className={`${styles.card} ${styles.card5}`}>
+                    <h3>5. Entrega Final e Suporte</h3>
+                </article>
             </div>
         </section>
     )
