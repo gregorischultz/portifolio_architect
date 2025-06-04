765def81163cdbbe06d05dd2c3f7d6036f052c20
Grégori <gregorischultz07@gmail.com>
2025-06-04 16:27:33 +0200

    adjustments to the id search in the backend, GET page projects and GET(id) made and some other adjustments regarding the connection with the database

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import Breadcrumb from "../componentes/ProjectPage/Breadcrumb";
 import ProjectFilters from "../componentes/ProjectPage/ProjectFilters";
 import ProjectGrid from "../componentes/ProjectPage/ProjectGrid";
 import NavBar from "../componentes/NavBar";
 
 export default function Projetos() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        async function fetchProjects() {
+            try {
+                const res = await fetch('/api/project');
+                const data = await res.json();
+                console.log(data)
+                setProjects(data);
+            } catch (err) {
+                console.log("Erro ao buscar projetos:", err);
+            }
+        }
+        fetchProjects();
+    }, []);
     return (
         <>
             <div><NavBar /></div>
             <Breadcrumb />
             <ProjectFilters />
-            <ProjectGrid projects={[]} />
+            <ProjectGrid projects={projects} />
         </>
     );
 }
