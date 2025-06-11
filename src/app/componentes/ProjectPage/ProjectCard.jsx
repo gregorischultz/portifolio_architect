78c8b10bea9fdad01ed6873467c9f5ff99fae64e
Grégori <gregorischultz07@gmail.com>
2025-06-11 18:36:53 +0200

    transforming project cards into buttons for project details

--- src/app/componentes/ProjectPage/ProjectCard.jsx
+++ src/app/componentes/ProjectPage/ProjectCard.jsx
@@ -1,12 +1,14 @@
-import styles from '@/app/styles/ProjectCard.module.css'
+import styles from '@/app/styles/ProjectCard.module.css';
+import Link from 'next/link';
 
 export default function ProjectCard({ project }) {
-
   const imgUrl = project.images?.[0]?.url;
 
   return (
-    <div className={styles.card}>
-      <img src={imgUrl} alt={project.title} className={styles.image} />
-    </div>
+    <Link href={`/project/${project.id}`} className={styles.cardLink}>
+      <div className={styles.card}>
+        <img src={imgUrl} alt={project.title} className={styles.image} />
+      </div>
+    </Link>
   );
 }
